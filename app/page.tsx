c859fae94b0c5d4e9b691d603914a266906e1889
akira.m1 <80916217+ae12321@users.noreply.github.com>
2024-02-15 01:48:45 +0900

    add shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-5xl text-blue-500">hello world</h1>
+      <Button className="w-full" variant={"link"}>
+        Click me
+      </Button>
     </main>
   );
 }
