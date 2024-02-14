c5fd320c8735a012fda1aa4653a100d64efef1d7
akira.m1 <80916217+ae12321@users.noreply.github.com>
2024-02-15 04:09:17 +0900

    dropdown

--- components/layout/Dropdown.tsx
+++ components/layout/Dropdown.tsx
@@ -1,5 +1,31 @@
+import { AlignJustify } from "lucide-react";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import links from "./links";
+import Link from "next/link";
 
 export default function Dropdown() {
-  return <div>Dropdown</div>;
+  return (
+    <div className="lg:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <AlignJustify />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {links.map((link) => (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="flex items-center gap-x-4">
+                {link.icon} <span>{link.title}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
