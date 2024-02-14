3dce6a6abeea5d4c612dc2dab9ba093c93b20524
akira.m1 <80916217+ae12321@users.noreply.github.com>
2024-02-15 03:58:50 +0900

    implement navbar, sidebar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Dropdown from "./Dropdown";
+import Theme from "./Theme";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
-  return <div className="border">Navbar</div>;
+  return (
+    <div className="bg-slate-300 p-4 flex items-center justify-between">
+      <div>
+        <Dropdown />
+      </div>
+      <div className="flex items-center gap-4">
+        <Theme />
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 }
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,39 @@
+"use client";
+
 import React from "react";
+import links from "./links";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import logoImage from "@/app/assets/logo.png";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  // const pathname = window.location.pathname;
+  const pathname = usePathname();
+
+  return (
+    <aside className="p-4 bg-slate-300 h-full">
+      <div className="w-full p-4">
+        <div className="flex items-center gap-4">
+          <Image src={logoImage} alt="logo" className="w-8 h-8" />
+          <h1 className=" font-bold">Memo App</h1>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-8 mt-16">
+        {links.map((link) => (
+          <Button
+            key={link.href}
+            asChild
+            variant={pathname === link.href ? "default" : "link"}
+            className="block w-full"
+          >
+            <Link href={link.href} className="flex items-center gap-x-4">
+              {link.icon} <span>{link.title}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
+    </aside>
+  );
 }
